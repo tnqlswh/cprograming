3d38751636ccee582ade4e77559ab93d72c1a00b
Gun bread <sbcho0304@naver.com>
2023-05-23 01:22:19 +0900

    대사 엔딩 제외 전부 입력 완료, 게임 구동 가능!!

--- main.c
+++ main.c
@@ -8,15 +8,15 @@ int main()
 	{
 		titleOpen();
 		menu();
-		printf("실행하길 원하는 번호를 입력하세요: ");
+		printf("실행하길 원하는 번호를 입력해주세요: ");
 		scanf_s("%d", &c);
 		while (1)
 		{
 			if (c == 1)//게임시작
 			{
 				CLR;
-				introPlay();
-				bathroom();
+				introPlay();//인트로
+				cellar();//지하실 입장
 				return 0;
 			}
 			else if (c == 2)//조작법
@@ -34,15 +34,21 @@ int main()
 				{
 					CLR;
 					introPlay();
-					return 0;
+					cellar();
+					break;//return 0 써야될지 break 써야될지... 일단 다 만들어보고 나서 플레이 해보고 결정.
 				}
 			}
 			else if (c == 3)//게임종료
 			{
 				exit(0);
 			}
+			else//선택지 이외의 숫자를 입력했을 경우
+			{
+				CLR;
+				break;
+			}
 		}
-		system("cls");
+		CLR;
 	}
 	return 0;
 }
