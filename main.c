58c0ba83b5746550a01c683f5b7bb3a8e0a8a855
Gun bread <sbcho0304@naver.com>
2023-05-24 02:26:35 +0900

    진짜 타이머 기능 빼고 전부 구현 완료했습니다..

--- main.c
+++ main.c
@@ -4,46 +4,45 @@ int main()
 {
 	int c;
 	init();
+	titleOpen();
+	scanf_s("%d", &c);
 	while (1)
 	{
-		titleOpen();
-		scanf_s("%d", &c);
-		while (1)
+		if (c == 1)//게임시작
 		{
-			if (c == 1)//게임시작
+			CLR;
+			introPlay();//인트로
+			cellar();//지하실 입장
+		}
+		else if (c == 2)//조작법
+		{
+			CLR;
+			infoDraw();
+			int infoChoose;
+			scanf_s("%d", &infoChoose);
+			if (infoChoose == 1)
 			{
 				CLR;
-				introPlay();//인트로
-				cellar();//지하실 입장
+				main();
 			}
-			else if (c == 2)//조작법
+			else if (infoChoose == 2)
 			{
 				CLR;
-				infoDraw();
-				int infoChoose;
-				scanf_s("%d", &infoChoose);
-				if (infoChoose == 1)
-				{
-					CLR;
-					break;
-				}
-				else if (infoChoose == 2)
-				{
-					CLR;
-					introPlay();
-					cellar();
-				}
-			}
-			else if (c == 3)//게임종료
-			{
-				exit(0);
-			}
-			else//선택지 이외의 숫자를 입력했을 경우
-			{
-				break;
+				introPlay();
+				cellar();
 			}
 		}
-		CLR;
+		else if (c == 3)//게임종료
+		{
+			exit(0);
+		}
+		else//선택지 이외의 숫자를 입력했을 경우
+		{
+			CLR;
+			main();
+		}
 	}
+		CLR;
 	return 0;
 }
+
